59753318414c9523f4770ecd937631aa0dbbe8ea
thanhphan <thanhvanphan@admicro.vn>
2024-09-20 16:13:49 +0700

    update 9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Index from "./pages/Index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />
+  },
+]);
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ frontend/src/pages/Index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div className='bg-[#18191b] min-h-screen w-full'>
+            <div className='bg-[#252627] shadow-md'>
+                <div className='w-[93%] m-auto py-3'>
+                    <div className='flex justify-between items-center'>
+                        <div className='w-[80px] h-[48px]'>
+                            <img className='w-full h-full' src="https://static.canva.com/web/images/8439b51bb7a19f6e65ce1064bc37c197.svg" alt="" />
+                        </div>
+                        <div className='flex gap-4'>
+                            <button className='py-2 w-[80px] text-center bg-blue-500 text-white translate-all hover:bg-blue-600 rounded-[5px] font-medium'>Sign in</button>
+                            <button className='py-2 w-[80px] text-center bg-purple-500 text-white translate-all hover:bg-purple-600 rounded-[5px] font-medium'>Sign up</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='w-full h-full justify-center items-center p-4'>
+                <div className='py-[168px] flex justify-center items-center flex-col gap-6'>
+                    <h2 className='text-5xl text-[#c7c5c5] font-bold'>What will you design today?</h2>
+                    <span className='text-[#aca9a9] text-2xl font-medium'>Canva makes it easy to create and share professional designs.</span>
+                    <button className='py-2 w-[200px] text-center bg-purple-500 text-white translate-all hover:bg-purple-600 rounded-[5px] font-medium'>Sign for free</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Index
